8b0233086371c362fb1ba7e55e72b017eb74edc3
arashabe <arashabedi88@gmail.com>
2024-11-23 15:48:48 +0100

    Update dir

--- SQL_XPath_Scripts/11_stored_procedure_creation.sql
+++ SQL_XPath_Scripts/11_stored_procedure_creation.sql
@@ -8,7 +8,7 @@ CREATE PROCEDURE InsertAndUpdateTables
 AS
 BEGIN
     DECLARE @FilePath NVARCHAR(255);
-    DECLARE @FolderPath NVARCHAR(255) = 'J:\DB2-project\NEW_FILES\\';
+    DECLARE @FolderPath NVARCHAR(255) = 'J:\DB2-project\COVID-19\\';
     DECLARE @SQL NVARCHAR(MAX);
     DECLARE @StudyID NVARCHAR(50);
 
@@ -17,7 +17,7 @@ BEGIN
 
     -- Use xp_cmdshell to get the list of files in the folder (ensure xp_cmdshell is enabled)
     INSERT INTO #Files (FileName)
-    EXEC xp_cmdshell 'dir J:\DB2-project\NEW_FILES\*.xml /b';
+    EXEC xp_cmdshell 'dir J:\DB2-project\COVID-19\*.xml /b';
 
     -- Remove any NULL values from the temporary table
     DELETE FROM #Files WHERE FileName IS NULL;
