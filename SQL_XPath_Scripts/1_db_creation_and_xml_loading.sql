d31c1f654d684913db98d414d87a4a1ef23569d4
arashabe <arashabedi88@gmail.com>
2024-12-02 10:50:51 +0100

    The query has been improved to handle null values while loading XML

--- SQL_XPath_Scripts/1_db_creation_and_xml_loading.sql
+++ SQL_XPath_Scripts/1_db_creation_and_xml_loading.sql
@@ -1,19 +1,19 @@
--- 1. Change to master database to avoid being in the target database when dropping it
+-- 1. Change to master database 
 USE master;
-
+Go
 -- 2. If the database exists, disconnect users and drop the database
 IF EXISTS (SELECT * FROM sys.databases WHERE name = 'DB2_covid_studies')
 BEGIN
     ALTER DATABASE DB2_covid_studies SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
     DROP DATABASE DB2_covid_studies;
 END
-
+GO
 -- 3. Create a new database
 CREATE DATABASE DB2_covid_studies;
-
+GO
 -- 4. Switch to the new database
 USE DB2_covid_studies;
-
+GO
 -- Enable xp_cmdshell to interact with the OS
 EXEC sp_configure 'show advanced options', 1;
 RECONFIGURE;
@@ -22,9 +22,13 @@ RECONFIGURE;
 
 -- Table for storing XML data from studies
 CREATE TABLE ClinicalStudies (
-    StudyID INT IDENTITY PRIMARY KEY,
+    AUTO_INCREMENT INT IDENTITY PRIMARY KEY,
     StudyXML XML
 );
+
+-- Drop the temporary table if it exists 
+IF OBJECT_ID('tempdb..#TempFiles', 'U') IS NOT NULL DROP TABLE #TempFiles;
+
 
 -- Temporary table to hold XML file names
 CREATE TABLE #TempFiles (FileName NVARCHAR(4000));
@@ -32,6 +36,10 @@ CREATE TABLE #TempFiles (FileName NVARCHAR(4000));
 -- Insert XML file names into the temporary table
 INSERT INTO #TempFiles (FileName)
 EXEC xp_cmdshell 'dir J:\DB2-project\COVID-19\*.xml /b';
+
+-- Remove NULL values 
+DELETE FROM #TempFiles WHERE FileName IS NULL;
+
 
 -- Verify the file names were inserted
 SELECT * FROM #TempFiles;
@@ -54,12 +62,12 @@ BEGIN
     -- Construct the full file path
     SET @fullPath = @folderPath + @fileName;
 
-    -- Build dynamic SQL to load the XML file into ClinicalStudies
+    -- Build SQL to load the XML file into ClinicalStudies
     SET @sql = 'INSERT INTO ClinicalStudies (StudyXML) 
                  SELECT CAST(BULK_COLUMN AS XML) 
                  FROM OPENROWSET(BULK ''' + @fullPath + ''', SINGLE_BLOB) AS T(BULK_COLUMN);';
 
-    -- Execute the dynamic SQL
+    -- Execute the SQL
     BEGIN TRY
         EXEC sp_executesql @sql;
     END TRY
